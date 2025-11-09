e42e14668a8184414daff72a9df9e6d9a73f54fa
griffith <xjulianbarragan@gmail.com>
2025-11-09 21:39:40 +0100

    Implemented pong game state, missing JSON send methods. Perfect reflection vector physics

--- backend/pong/game.py
+++ backend/pong/game.py
@@ -1,9 +1,11 @@
 from const import (
         BALL_SIZE,
+        BALL_VELOCITY,
         BUFFER,
         CANVAS_HEIGHT,
         CANVAS_WIDTH,
         PADDLE_HEIGHT,
+        PADDLE_VELOCITY,
         PADDLE_WIDTH,
 )
 
@@ -21,26 +23,29 @@ class Ball:
     def __init__(self) -> None:
         self.x: float
         self.y: float
-        self.heading: float # direction in degrees
-        self.speed: float = 7.0
 
     def __repr__(self) -> str:
-        return f"Ball with pos=({self.x}, {self.y}), velocity={self.speed} and heading={self.heading}"
+        return f"Ball with pos=({self.x}, {self.y})"
 
 class Paddle:
-    def __init__(self, x: int, y: int, vel: int = 5) -> None:
+    def __init__(self, x: int, y: int) -> None:
         self.x: int = x
         self.y: int = y
-        self.vel: int = vel 
 
     def __repr__(self) -> str:
-        return f"Paddle with pos=({self.x}, {self.y}) and velocity={self.vel}"
+        return f"Paddle with pos=({self.x}, {self.y})"
 
 class Pong():
+    """Class representing the Pong game state and logic."""
     def __init__(self) -> None:
-        self.human: Paddle = Paddle(PADDLE_WIDTH, (CANVAS_HEIGHT // 2))
-        self.agent: Paddle = Paddle(CANVAS_WIDTH - BUFFER + PADDLE_WIDTH, (CANVAS_HEIGHT // 2))
+        self.init_human_pos: tuple[int, int] = (PADDLE_WIDTH, CANVAS_HEIGHT // 2)
+        self.init_agent_pos: tuple[int, int] = (CANVAS_WIDTH - BUFFER - PADDLE_WIDTH, CANVAS_HEIGHT // 2)
+        self.human: Paddle = Paddle(*self.init_human_pos)
+        self.agent: Paddle = Paddle(*self.init_agent_pos)
+        self.paddle_speed: int = PADDLE_VELOCITY
         self.ball: Ball = Ball()
+        self.ball_speed: int = BALL_VELOCITY
+        self.ball_heading: float
         self.dirvector: np.ndarray
         self.score: dict[str, int] = {"human": 0, "agent": 0}
         self.winning_score: int = 3
@@ -63,15 +68,28 @@ class Pong():
         # Set x always in the middle
         self.ball.x = CANVAS_WIDTH // 2
         # Start the ball orthogonally to either left or right paddle
-        self.ball.heading = 0. if np.random.randint(0, 2) == 1 else 180.
+        self.ball_heading = 0. if np.random.randint(0, 2) == 1 else 180.
         # Add slight scale withing +/- 45 degrees
-        self.ball.heading = self.ball.heading + np.random.uniform(-45., 45.)
-        # TEMPORARY
-        self.ball.heading = 80.
+        self.ball_heading = self.ball_heading + np.random.uniform(-45., 45.)
+        # Avoid perfect vertical angles (perfect reflection edge case)
+        if self.ball_heading == 0. or self.ball_heading == 180.:
+            self.ball_heading = self.ball_heading + 10.
         self.dirvector = np.array([
-                np.cos(self.ball.heading*self.ball.x),
-                np.sin(self.ball.heading*self.ball.y)
+                np.cos(np.radians(self.ball_heading)),
+                np.sin(np.radians(self.ball_heading))
                 ])
+
+    def _update_direction_vector(self, normal_dir: str) -> None:
+        """
+        Assuming perfect reflection, this computes the updated direction vector reflected on one of the 4 normals.
+        Parameters
+        ----------
+        normal_dir : str
+            One of "top", "bottom", "left", "right" indicating which normal to reflect on.
+        """
+        assert normal_dir in self.normal.keys(), "Normal direction must be one of 'top', 'bottom', 'left', 'right'."
+        normal = self.normal[normal_dir]
+        self.dirvector =  self.dirvector - 2*np.dot(self.dirvector, normal)*normal
 
     def move_paddle(self, human: bool, dir: int) -> None:
         """
@@ -89,34 +107,14 @@ class Pong():
         I tried to do cool smart logic, but it didn't work. Did bare minimum viable product instead.
         """
         assert dir == -1 or dir == 1, "Up and down should be given by -1 and 1 respectively."
-        if human and dir == 1: # Try to move human paddle down
-            if self.human.y + PADDLE_HEIGHT < CANVAS_HEIGHT:
-                new_pos = self.human.y + dir*self.human.vel
-                if new_pos + PADDLE_HEIGHT > CANVAS_HEIGHT:
-                    self.human.y = CANVAS_HEIGHT - PADDLE_HEIGHT
-                else:
-                    self.human.y = new_pos
-        elif human and dir == -1: # Try to move human paddle up
-            if self.human.y - PADDLE_HEIGHT > 0:
-                new_pos = self.human.y + dir*self.human.vel
-                if new_pos - PADDLE_HEIGHT < 0: 
-                    self.human.y = PADDLE_HEIGHT
-                else:   
-                    self.human.y = new_pos
-        elif not human and dir == 1: # Try to move agent paddle down
-            if self.agent.y + PADDLE_HEIGHT < CANVAS_HEIGHT:
-                new_pos = self.agent.y + dir*self.agent.vel
-                if new_pos + PADDLE_HEIGHT > CANVAS_HEIGHT:
-                    self.agent.y = CANVAS_HEIGHT - PADDLE_HEIGHT
-                else:
-                    self.agent.y = new_pos
-        elif not human and dir == -1: # Try to move agent paddle up
-            if self.agent.y - PADDLE_HEIGHT > 0:
-                new_pos = self.agent.y + dir*self.agent.vel
-                if new_pos - PADDLE_HEIGHT < 0: 
-                    self.agent.y = PADDLE_HEIGHT
-                else:   
-                    self.agent.y = new_pos
+        player = self.human if human else self.agent
+
+        min_y = PADDLE_HEIGHT // 2
+        max_y = CANVAS_HEIGHT - (PADDLE_HEIGHT // 2)
+
+        new_y = player.y + dir*self.paddle_speed
+        player.y = int(np.clip(new_y, min_y, max_y))
+
 
     def move_ball(self) -> None:
         """
@@ -134,42 +132,42 @@ class Pong():
         hitting_human_paddle = within_paddle_bound(self.ball.y, self.human.y) and \
                             self.ball.x <= self.human.x + PADDLE_WIDTH + BALL_SIZE
         hitting_agent_paddle = within_paddle_bound(self.ball.y, self.agent.y) and \
-                            self.ball.x >= self.agent.x - BALL_SIZE # y is leftmost on agent paddle
+                            self.ball.x >= self.agent.x - BALL_SIZE
         hitting_paddle = hitting_agent_paddle or hitting_human_paddle
+
         # Compute new direction vectors for each of 4 cases
         if hitting_top and not hitting_paddle:
-            print("top hit!")
-            self.dirvector = 2*np.dot(-self.dirvector, self.normal["top"]) + self.dirvector
+            self._update_direction_vector("top")
         if hitting_bottom and not hitting_paddle:
-            print("bottom hit!")
-            # TODO: Fix buttom hit reflection, potentially others
-            self.dirvector = 2*np.dot(-self.dirvector, self.normal["bottom"]) + self.dirvector
+            self._update_direction_vector("bottom")
         if hitting_human_paddle:
-            print("human paddle hit!")
-            self.dirvector = 2*np.dot(-self.dirvector, self.normal["left"]) + self.dirvector
+            self._update_direction_vector("left")
         if hitting_agent_paddle:
-            print("agent paddle hit!")
-            self.dirvector = 2*np.dot(-self.dirvector, self.normal["right"]) + self.dirvector
+            self._update_direction_vector("right")
+
         # Handle scoring
-        if self.ball.x >= CANVAS_WIDTH - (BUFFER + PADDLE_WIDTH) and not hitting_paddle: # human point
-            self.score["human"] += 1
-            print(f"Score update! Human: {self.score['human']} | Agent: {self.score['agent']}")
-            if self.score["human"] >= self.winning_score:
-                self.game_over = True
+        scored_left = self.ball.x <= (BUFFER + PADDLE_WIDTH) and not hitting_paddle
+        scored_right = self.ball.x >= CANVAS_WIDTH - (BUFFER + PADDLE_WIDTH) and not hitting_paddle
+        if scored_left or scored_right:
+            logger.info(f"Score update! Human: {self.score['human']} | Agent: {self.score['agent']}")
+            # Update scores
+            if scored_left:
+                self.score["agent"] += 1
+                scored_by = "agent"
+            else:
+                self.score["human"] += 1
+                scored_by = "human"
+
+            # Update paddle positions
+            self.human.y = self.init_human_pos[1]
+            self.agent.y = self.init_agent_pos[1]
             self._get_ball_start_position()
-        elif self.ball.x <= (BUFFER + PADDLE_WIDTH) and not hitting_paddle:
-            self.score["agent"] += 1
-            print(f"Score update! Human: {self.score['human']} | Agent: {self.score['agent']}")
-            if self.score["agent"] >= self.winning_score:
+
+            # Check for game over
+            if self.score[scored_by] >= self.winning_score:
                 self.game_over = True
-            self._get_ball_start_position()
+                logger.info(f"Game Over! {scored_by.capitalize()} wins!")
+
         # Update position
-        self.ball.x += self.ball.speed*self.dirvector[0]
-        self.ball.y += self.ball.speed*self.dirvector[1]
-
-
-if __name__ == "__main__":
-    game = Pong()
-    while not game.game_over:
-        print(f"Ball currently at position ({game.ball.x}, {game.ball.y})")
-        game.move_ball()
+        self.ball.x += self.ball_speed*self.dirvector[0]
+        self.ball.y += self.ball_speed*self.dirvector[1]
